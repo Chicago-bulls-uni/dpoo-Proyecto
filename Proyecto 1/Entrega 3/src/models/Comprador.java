c3fae6fb822b78c8f7bbd648c7803be9fe4e8ad0
srojasre <s.rojasr2@uniandes.edu.co>
2024-05-11 12:09:51 -0500

    opa

--- Proyecto 1/Entrega 3/src/models/Comprador.java
+++ Proyecto 1/Entrega 3/src/models/Comprador.java
@@ -1,85 +1,73 @@
 package models;
 
-import java.util.List;
-
+import java.util.Date;
+import java.util.TreeMap;
 public class Comprador {
-	
-	private int idComprador;
-	private String nombre;
-	private String contacto;
-	
-	private Compra compra;
-	private double limiteCompra;
-	private boolean verificado;
-	
-	public Comprador (int idCompradorP, String nombreP, String contactoP, Compra compraP , double limiteCompraP, boolean verificadoP) {
-		this.setIdComprador(idCompradorP);
-		this.setNombre(nombreP);
-		
-		this.setContacto(contactoP);
-		this.setCompra(compraP);
-		this.setLimiteCompra(limiteCompraP);
-		this.setVerificado(verificadoP);
-	
-	}
+    
+    private int idComprador;
+    private String nombre;
+    private String contacto;
+    
+    private TreeMap<Date, Pieza> comprasRealizadas;
+    private double limiteCompra;
+    private boolean verificado;
+    
+    public Comprador (int idCompradorP, String nombreP, String contactoP,  double limiteCompraP, boolean verificadoP) {
+        this.setIdComprador(idCompradorP);
+        this.setNombre(nombreP);
+        this.comprasRealizadas= new TreeMap<Date, Pieza>();
+        this.setContacto(contactoP);
+        this.verificado=false;
+        this.setLimiteCompra(limiteCompraP);
+        this.setVerificado(verificadoP);
+    
+    }
 
-	public int getIdComprador() {
-		return idComprador;
-	}
+    public int getIdComprador() {
+        return idComprador;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public String getContacto() {
-		return contacto;
-	}
+    public String getContacto() {
+        return contacto;
+    }
 
-	public List<Compra> getComprasRealizadas() {
-		return comprasRealizadas;
-	}
+    public TreeMap<Date, Pieza> getComprasRealizadas() {
+        return comprasRealizadas;
+    }
 
-	public void setIdComprador(int idComprador) {
-		this.idComprador = idComprador;
-	}
+    public void setIdComprador(int idComprador) {
+        this.idComprador = idComprador;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public void setContacto(String contacto) {
-		this.contacto = contacto;
-	}
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
 
-	public void setComprasRealizadas(List<Compra> comprasRealizadas) {
-		this.comprasRealizadas = comprasRealizadas;
-	}
+    
 
-	public Compra getCompra() {
-		return compra;
-	}
+    
 
-	public void setCompra(Compra compra) {
-		this.compra = compra;
-	}
+    public double getLimiteCompra() {
+        return limiteCompra;
+    }
 
-	public double getLimiteCompra() {
-		return limiteCompra;
-	}
+    protected void setLimiteCompra(double limiteCompra) {
+        this.limiteCompra = limiteCompra;
+    }
 
-	public void setLimiteCompra(double limiteCompra) {
-		this.limiteCompra = limiteCompra;
-	}
+    public boolean isVerificado() {
+        return verificado;
+    }
 
-	public boolean isVerificado() {
-		return verificado;
-	}
-
-	public void setVerificado(boolean verificado) {
-		this.verificado = verificado;
-	}
-	
-	
-	
-	
+    public void setVerificado(boolean verificado) {
+        this.verificado = verificado;
+    }
 }
